2ea3cada0c0504fbb59e146742ccb3618423c0f5
Somin Kang <somin.kang.91@gmail.com>
2020-08-20 19:48:52 -0700

    Updates

---
+++ precache-manifest.49c6f0a4b47aea713e17358589ba8cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d2e7d4920ddfb5a55ad1fe437b3335",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f8626c6bd3073d592c0b",
+    "url": "/Clicky-Game/static/css/main.61a9fac0.chunk.css"
+  },
+  {
+    "revision": "3572bc3e2b222b86726b",
+    "url": "/Clicky-Game/static/js/2.59f7b85d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.59f7b85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8626c6bd3073d592c0b",
+    "url": "/Clicky-Game/static/js/main.67e0c8fb.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  },
+  {
+    "revision": "29880620ed3f8a147a36e10b32523f02",
+    "url": "/Clicky-Game/static/media/banner3.29880620.jpg"
+  }
+]);
